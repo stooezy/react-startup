de1d707f6a055d69b8b529c1cf59a0e115164e81
Yoga Permana <yopernurr.s@gmail.com>
2022-11-05 10:00:52 +0700

    setup: file based routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { Suspense } from 'react'
+import {
+  BrowserRouter as Router,
+  useRoutes,
+} from 'react-router-dom'
+
+import routes from '~react-pages'
+
 import './index.css'
 
+const App = () => {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      {useRoutes(routes)}
+    </Suspense>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 )
