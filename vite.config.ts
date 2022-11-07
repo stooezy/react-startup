f86a14062406d1cd919c0ea46914ffb8ec28560d
Yoga Permana <yopernurr.s@gmail.com>
2022-11-07 09:47:49 +0700

    setup: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import Pages from 'vite-plugin-pages';
 import AutoImport from 'unplugin-auto-import/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -28,5 +29,6 @@ export default defineConfig({
         enabled: true,
       },
     }),
+    VitePWA({ registerType: 'autoUpdate' }),
   ],
 });
